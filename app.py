df6ac4267cb22690514948f51806d830541f6a2d
alvarezmike <kerer74@gmail.com>
2023-01-18 15:52:44 -0500

    updated dates shown
    Removed 'longBusinessSummary' because an error was produced. May check this later on. Also, update yfinance package

--- app.py
+++ app.py
@@ -3,24 +3,25 @@ import streamlit as st
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 # For more emojis code https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Stock Price", page_icon="::bar_chart:")
-st.header("Amazon Stock Price Web App")
+st.header("Amazon Stock Price Analysis")
 
 # define the ticker symbol
 tickerSymbol = 'AMZN'
 # get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 # get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2022-10-24')
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2023-01-16')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 # ticker company overview
-ticker_summary = tickerData.info['longBusinessSummary']
-st.info(ticker_summary)
+# ticker_summary = tickerData.info['longBusinessSummary']
+# st.info(ticker_summary)
+
 
 st.write("""
 Shown are the stock closing price and volume of Amazon!
 """)
-st.caption("Graph start=2010-5-31, end=2022-10-24")
+st.caption("Graph start=2010-5-31, end=2023-01-16")
 
 # charts displayed
 st.line_chart(tickerDf.Close)
